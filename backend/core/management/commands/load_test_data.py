86adb3f8a1f44d1eb7c51daeb0efce2bc4afc4c9
Marina <vorona.2003.bu@yandex.ru>
2023-03-19 09:20:09 +0300

    Load_test_data command upgrade

--- backend/core/management/commands/load_test_data.py
+++ backend/core/management/commands/load_test_data.py
@@ -8,7 +8,18 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import make_password
 
 from foodgram.settings import BASE_DIR
-from recipes.models import Ingredient
+from recipes.models import (
+    Ingredient,
+    Tag,
+    Category,
+    Selection,
+    Cuisine,
+    Recipe,
+    SelectionRecipe,
+    RecipeImage,
+    RecipeIngredient,
+    Favorite
+)
 # from users.models import Follow
 
 User = get_user_model()
@@ -19,11 +30,16 @@ ALREDY_LOADED_ERROR_MESSAGE = """Если вам нужно загрузить �
 готовой к загрузке данных.
 """
 
-def image64_decode(imagebase64):
+def image64_decode(imagebase64, *args):
+    if not imagebase64:
+        return imagebase64
     format, imgstr = imagebase64.split(';base64,')
     ext = format.split('/')[-1]  
     data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
     return data
+
+def get_instance(pk, entity):
+    return entity[0].objects.get(pk=pk)
 
 
 class Command(BaseCommand):
@@ -31,11 +47,45 @@ class Command(BaseCommand):
     data_path = os.path.join(BASE_DIR, 'data')
     file_names = {
         Ingredient: 'ingredients.json',
-        User: 'users.json'
+        User: 'users.json',
+        Tag: 'tags.json',
+        Category: 'categories.json',
+        Selection: 'selections.json',
+        Cuisine: 'cuisines.json',
+        Recipe: 'recipes.json',
+        SelectionRecipe: 'selectionsrecipes.json',
+        RecipeImage: 'recipesimages.json',
+        RecipeIngredient: 'recipesingreds.json',
+        Favorite: 'favorites.json',
     }
-    fields_to_convert = {
-        Ingredient: {"image": image64_decode},
-        User: {"password": make_password}
+    to_convert = {
+        Ingredient: {"image": (image64_decode,)},
+        User: {"password": (make_password,)},
+        Selection: {
+            "cover": (image64_decode,),
+            "author": (get_instance, User),
+            "category": (get_instance, Category)
+        },
+        Recipe: {"author": (get_instance, User)},
+        SelectionRecipe: {
+            "selection": (get_instance, Selection),
+            "recipe": (get_instance, Recipe)
+        },
+        RecipeImage: {
+            "recipe": (get_instance, Recipe),
+            "image": (image64_decode,)
+        },
+        RecipeIngredient: {
+            "recipe": (get_instance, Recipe),
+            "ingredient": (get_instance, Ingredient)
+        },
+        Favorite: {
+            "recipe": (get_instance, Recipe),
+            "user": (get_instance, User)
+        }
+    }
+    to_set = {
+        Recipe: "tags",
     }
 
     def data_already_loaded(self, entity):
@@ -45,18 +95,19 @@ class Command(BaseCommand):
             return True
         return False
 
-    def field_conversion(self, entity, data, field_name, func):
+    def field_conversion(self, entity, data, field_name, func_info):
+        func, *args = func_info
         objs = []
+
         try:
-            for item in data:
-                field = item.pop(field_name)
-                item[field_name] = func(field)
-                objs.append(entity(**item))
-        except Exception:
-            objs=[entity(**item) for item in data]
+            for obj_data in data:
+                value = obj_data.pop(field_name)
+                obj_data[field_name] = func(value, args)
+                objs.append(obj_data)
+        except Exception as e:
             print(
                 f'{entity.__name__}: Во время преобразования поля {field_name} '
-                f'произошла ошибка: {Exception} \n Данные не преобразованы.'
+                f'произошла ошибка: {e} \n Данные не преобразованы.'
             )
         return objs
 
@@ -74,10 +125,26 @@ class Command(BaseCommand):
             loaded_data = json.load(data)
 
             objs=loaded_data
-            for field_name, func in self.fields_to_convert[entity].items():
-                objs = self.field_conversion(entity, objs, field_name, func)
 
-            entity.objects.bulk_create(objs)
+            if entity in self.to_convert:
+                for field_name, func_info in self.to_convert[entity].items():
+                    objs = self.field_conversion(
+                        entity, objs, field_name, func_info
+                    )
+
+            if not objs:
+                print('Данные не загружены.\n')
+                return
+            
+            if entity in self.to_set:
+                field_name = self.to_set[entity]
+                for obj in objs:
+                    values = obj.pop(field_name) # try except
+                    instance = entity.objects.create(**obj)
+                    getattr(instance, field_name).set(values)
+
+            else:
+                entity.objects.bulk_create([entity(**obj) for obj in objs])
 
             print(f'{entity.__name__}: Данные из файла {file_name} загружены.\n')
 
